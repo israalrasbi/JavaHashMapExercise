f76236f575715676d9598b3ede80f5acc51a8dea
israalrasbi <139228011+israalrasbi@users.noreply.github.com>
2024-10-20 13:46:22 +0400

    first commit

---
+++ AdjustBasedOnIngredients/AdjustIngredients.java
@@ -0,0 +1,25 @@
+import java.util.HashMap;
+
+public class AdjustIngredients {
+    public static void main(String[] args) {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("spinach", "dirt");
+        map.put("ice cream", "cherry");
+        System.out.println("Map after modifying: "+modifyMap(map));
+
+    }
+
+    public static HashMap<String, String> modifyMap (HashMap<String, String> map){
+        //check if ice cream exist
+        if(map.containsKey("ice cream")){
+            //add the new element
+            map.put("yogurt", map.get("ice cream"));
+        }
+        //check if spinach exist
+        if(map.containsKey("spinach")){
+            map.put("spinach", "nuts");
+        }
+        return map;
+    }
+
+}
