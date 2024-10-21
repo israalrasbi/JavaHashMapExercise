9156ed1c22b87afdb74f9fbe2e1c072c3a8c0e8a
israalrasbi <139228011+israalrasbi@users.noreply.github.com>
2024-10-21 12:51:09 +0400

    second commit
    changed the return type of modifyMap() function, and added a helper function.

--- ToppingShenanigans/ToppingShenanigans.java
+++ ToppingShenanigans/ToppingShenanigans.java
@@ -1,25 +1,26 @@
 import java.util.HashMap;
+import java.util.Map;
 
 public class ToppingShenanigans {
     public static void main(String[] args) {
         HashMap<String, String> map = new HashMap<>();
         map.put("salad", "oil");
         map.put("potato", "ketchup");
+        System.out.println("Map before modifying: " + map);
         System.out.println("Map after modifying: "+modifyMap(map));
     }
 
-    public static HashMap<String, String> modifyMap (HashMap<String, String> map){
-        //check if key exist
-        if(map.containsKey("potato")){
-            //add the new element
-            map.put("fries", map.get("potato"));
-        }
-        //check if key exist
-        if(map.containsKey("salad")){
-            //add the new element
-            map.put("spinach",map.get("salad"));
-        }
+    public static Map<String, String> modifyMap (HashMap<String, String> map){
+        addNewTopping(map, "potato", "fries");
+        addNewTopping(map, "salad", "spinach");
         return map;
+    }
+
+    private static void addNewTopping(HashMap<String, String> map, String existingKey, String newKey) {
+        if (map.containsKey(existingKey)) {
+            //add the new key with the existing key value
+            map.put(newKey, map.get(existingKey));
+        }
     }
 
 }
